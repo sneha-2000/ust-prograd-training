3a5f189e98c62e1f77cffa170c070c0a0831ea69
Sneha <snehapradeep915@gmail.com>
2021-12-09 17:52:17 +0530

    Arithmetic Opertaions on Codes for Day-14

--- TDD-3/src/main/java/UnitsConvertor.java
+++ TDD-3/src/main/java/UnitsConvertor.java
@@ -1,45 +1,45 @@
 public class UnitsConvertor {
-        public double convert(double value, String  from, String to) {
-            if (value < 0) throw new IllegalArgumentException("value cannot be negative");
+    public double convert(double value, String  from, String to) {
+        if (value < 0) throw new IllegalArgumentException("value cannot be negative");
 
-            switch (to) {
-                case "cm":
-                    return toCentiMeter(value, from);
-                case "m":
-                    return toMeter(value, from);
-                case "km":
-                    return toKiloMeter(value, from);
-                default:
-                    throw new IllegalArgumentException("units either not valid or not supported");
-            }
+        switch (to) {
+            case "cm":
+                return toCentiMeter(value, from);
+            case "m":
+                return toMeter(value, from);
+            case "km":
+                return toKiloMeter(value, from);
+            default:
+                throw new IllegalArgumentException("units either not valid or not supported");
         }
+    }
 
-        public double toCentiMeter(double value, String unit) {
-            switch (unit) {
-                case "cm":
-                    return value;
-                case "m":
-                    return value * 100;
-                case "km":
-                    return value * 100_000;
-                default:
-                    throw new IllegalArgumentException("units either not valid or not supported");
-            }
+    public double toCentiMeter(double value, String unit) {
+        switch (unit) {
+            case "cm":
+                return value;
+            case "m":
+                return value * 100;
+            case "km":
+                return value * 100_000;
+            default:
+                throw new IllegalArgumentException("units either not valid or not supported");
         }
+    }
 
-        public double toMeter(double value, String unit) {
-            switch (unit){
+    public double toMeter(double value, String unit) {
+        switch (unit){
 
             case "cm":
-            return value * 0.01;
+                return value * 0.01;
             case "m":
-            return value;
+                return value;
             case "km":
-            return value * 1000;
+                return value * 1000;
             default:
-            throw new IllegalArgumentException("units either not valid or not supported");
+                throw new IllegalArgumentException("units either not valid or not supported");
         }
-}
+    }
 
     public double toKiloMeter(double value, String unit) {
         switch (unit) {
@@ -54,4 +54,5 @@ public class UnitsConvertor {
         }
     }
 }
+
 
