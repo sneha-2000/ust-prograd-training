f099b1e0e78067cc065a51f812f7ab3a413216e2
Sneha <snehapradeep915@gmail.com>
2021-12-14 15:40:32 +0530

    Codes for day-17

--- DAY-17/src/main/java/CoinToss.java
+++ DAY-17/src/main/java/CoinToss.java
@@ -1,2 +1,31 @@
-package PACKAGE_NAME;public class CoinToss {
+public class CoinToss {
+    double head;
+    double tail;
+
+    public double whenProbabilityIsNegative(double probability) {
+        if (probability < 0) {
+            throw new IllegalArgumentException("Probability can't be negative");
+        }
+        return 0;
+    }
+
+    public double probabilityOfHead(){
+        return head= 0.5;
+    }
+
+    public double probabilityOfTail(){
+        return tail=0.5;
+    }
+
+    public double probabilityOfTwoEvents(){
+        return probabilityOfHead() * probabilityOfTail();
+    }
+
+    public double probabilityOfNotGettingTwoEventsTogether(){
+        return 1 - probabilityOfTwoEvents();
+    }
+
+    public double probabilityOfGettingEitherTwoEvents(){
+        return probabilityOfHead() + probabilityOfTail() - probabilityOfTwoEvents();
+    }
 }
--- DAY-17/src/test/java/CoinTossTest.java
+++ DAY-17/src/test/java/CoinTossTest.java
@@ -1,4 +1,82 @@
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CoinTossTest {
-  
+
+    CoinToss coinToss= new CoinToss();
+
+    @Nested // heads=tail
+    public class HeadsEqualToTails{
+
+        @Test
+        public void checkIfProbabilityOfHeadsAndTailsIsPointFive(){
+            assertEquals(coinToss.probabilityOfHead(),coinToss.probabilityOfTail());
+
+        }
+
+    }
+
+    @Nested //  probability of two events occurring together
+    public class TwoCoinEventsOccurringTogether{
+
+        @Test
+        public void checkIfProbabilityOfHeadIsPointTwoFive(){
+            assertEquals(0.25,coinToss.probabilityOfTwoEvents());
+        }
+
+        @Test
+        public void checkIfProbabilityOfTailIsPointTwoFive(){
+            assertEquals(0.25,coinToss.probabilityOfTwoEvents());
+        }
+
+        @Test
+        public void checkIfProbabilityOfHeadAndProbabilityOfTailIsPointTwoFive(){
+            assertEquals(0.25,coinToss.probabilityOfTwoEvents());
+        }
+
+        @Test
+        public void checkIfProbabilityOfTailAndProbabilityOfHeadIsPointTwoFive(){
+            assertEquals(0.25,coinToss.probabilityOfTwoEvents());
+        }
+
+    }
+
+    @Nested // probability of an event not occurring
+    public class TwoCoinsEventsNotOccurringTogether{
+
+        @Test
+        public void checkIfProbabilityOfTwoCoinSEventsNotOccurringTogether(){
+            assertEquals(0.75, coinToss.probabilityOfNotGettingTwoEventsTogether());
+        }
+    }
+
+    @Nested // probability of either two events occurring
+    public class EitherTwoCoinsEventsOccurring{
+
+        @Test
+        public void checkIfProbabilityOfEitherTwoCoinsEventsOccurring(){
+            assertEquals(0.75,coinToss.probabilityOfGettingEitherTwoEvents());
+        }
+
+        @Test
+        public void throwsIllegalArgumentExceptionWhenProbabilityIsNegative(){
+            assertThrows(IllegalArgumentException.class, () -> coinToss.whenProbabilityIsNegative(-1.6));
+        }
+    }
 }
+
+
+
+
+
+
+
+
+
+
+//    As a statistician, I want to be able to know that the probability of a heads in a coin toss is equal to the probability of a tails in a coin toss.
+//        As a statistician, I want to be able to know the probability of two events occurring together.
+//        As a statistician, I want to be able to know the probability of an event not occurring.
+//        As a statistician, I want to be able to know the probability of either two events occurring.
