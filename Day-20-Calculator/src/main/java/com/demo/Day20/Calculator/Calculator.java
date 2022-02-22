6973e4c3b8ca385c67e122ce16361db6e94a2082
Sneha <snehapradeep915@gmail.com>
2022-02-22 23:01:08 +0530

    Day-20

--- Day-20-Calculator/src/main/java/com/demo/Day20/Calculator/Calculator.java
+++ Day-20-Calculator/src/main/java/com/demo/Day20/Calculator/Calculator.java
@@ -1,2 +1,30 @@
-package com.demo.Day20.Calculator;public class Calculator {
-}
+package com.demo.Day20.Calculator;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Calculator {
+
+        @Value("${num1}")
+        public double num1;
+        @Value("${num2}")
+        public double num2;
+
+        public double addition(){
+            System.out.println("Addition of " + num1 + " and " + num2 + "= ");
+            return (num1 + num2);
+        }
+        public double subtraction(){
+            System.out.println("Subtraction of " + num1 + " and " + num2 + "= ");
+            return (num1 - num2);
+        }
+        public double multiplication(){
+            System.out.println("Multiplication of " + num1 + " and " + num2 + "= ");
+            return (num1 * num2);
+        }
+        public double division(){
+            System.out.println("Division of " + num1 + " and " + num2 + "= ");
+            return (num1 / num2);
+        }
+    }
